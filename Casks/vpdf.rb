f399343783c7b796b2844c37c199656952da75ea
Seongil Park <zihado@gmail.com>
2026-07-09 20:18:03 +0900

    feat: vpdf 1.0.1

--- Casks/vpdf.rb
+++ Casks/vpdf.rb
@@ -1,6 +1,6 @@
 cask "vpdf" do
-  version "1.0.0"
-  sha256 "39af651e1e81e8725916c385d8e815f9b3e3c88e5a35e07155ce782f47d1a586"
+  version "1.0.1"
+  sha256 "be74bb5731f16567773d65111fbfa3ed12eb6c649050da59b53e6c39232f5687"
 
   url "https://github.com/seongilp/vpdf/releases/download/v#{version}/vpdf-#{version}.dmg"
   name "vpdf"
